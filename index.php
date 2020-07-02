3ffa7efb5edda97b7f532d643476e318df23a1d9
hashibul218 <hashibul218@outlook.com>
2020-07-02 23:46:31 +0600

    Student app form ready

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Add new student</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Add Student</h2>
+				<form action="" method="POST">
+					<div class="form-group">
+						<label for="">Name</label>
+						<input name="name" class="form-control" type="text" placeholder="Name">
+					</div>
+					<div class="form-group">
+						<label for="">Email</label>
+						<input name="email" class="form-control" type="text" placeholder="Email">
+					</div>
+					<div class="form-group">
+						<label for="">Cell</label>
+						<input name="cell" class="form-control" type="text" placeholder="Cell no.">
+					</div>
+					<div class="form-group">
+						<label for="">Age</label>
+						<input name="age" class="form-control" type="text" placeholder="Age">
+					</div>
+					<div class="form-group">
+						<input name="submit" class="btn btn-primary" type="submit" value="Add Student">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
