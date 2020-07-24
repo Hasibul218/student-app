2654179201485ddb7e646e764298d79f2d04f344
hashibul218 <hashibul218@outlook.com>
2020-07-24 09:16:03 +0600

    config the app

--- index.php
+++ index.php
@@ -16,9 +16,13 @@
 		 */
 		if (isset($_POST['submit'])) {
 			$name = $_POST['name'];
+			$uname = $_POST['uname'];
 			$email = $_POST['email'];
 			$cell = $_POST['cell'];
 			$age = $_POST['age'];
+			$location = $_POST['location'];
+			$gender = $_POST['gender'];
+
 
 			/**
 			 * Name Empty Check
@@ -79,6 +83,10 @@
 						<input name="name" class="form-control" type="text" placeholder="Name" value="<?php oldData('name') ?>">
 					</div>
 					<div class="form-group">
+						<label for="">Username</label>
+						<input name="name" class="form-control" type="text" placeholder="Username" value="<?php oldData('uname') ?>">
+					</div>
+					<div class="form-group">
 						<label for="">Email</label>
 						<input name="email" class="form-control" type="text" placeholder="Email" value="<?php oldData('email') ?>">
 					</div>
@@ -89,6 +97,31 @@
 					<div class="form-group">
 						<label for="">Age</label>
 						<input name="age" class="form-control" type="text" placeholder="Age" value="<?php oldData('age') ?>">
+					</div>
+					<div class="form-group">
+						<label for="">Location</label>
+						<select name="" id="" class="form-control">
+							<option value="">-Select-</option>
+							<option value="Mirpur">Mirpur</option>
+							<option value="Dhanmondi">Dhanmondi</option>
+							<option value="Khilkhet">Khilkhet</option>
+							<option value="Nikunja">Nikunja</option>
+							<option value="Banani">Banani</option>
+						</select>
+					</div>
+					<div class="form-group">
+						<label for="">Gender</label>
+						<br>
+						<input name="gender" type="radio" value="Male" id="male"><label for="male">&nbsp Male</label>
+						<input name="gender" type="radio" value="Female" id="female"><label for="female">&nbsp Female</label>
+					</div>
+					<div class="form-group">
+						<label for="">Photo</label>
+						<br>
+						<input type="file" name="file" class="form-control">
+					</div>
+					<div class="form-group">
+						<input type="checkbox" id="publish"><label for="publish">&nbsp Published</label>
 					</div>
 					<div class="form-group">
 						<input name="submit" class="btn btn-primary" type="submit" value="Add Student">
